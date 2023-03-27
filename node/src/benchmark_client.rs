82bcd80ce652fa930560962af80fe2db9d90fb5d
liuky <stephenkobylky@gmail.com>
2023-03-27 21:44:58 +0900

    improve tests

--- node/src/benchmark_client.rs
+++ node/src/benchmark_client.rs
@@ -4,7 +4,8 @@
 use bytes::{BufMut as _, BytesMut};
 use clap::{crate_name, crate_version, App, AppSettings};
 use eyre::Context;
-use futures::{future::join_all, StreamExt};
+use futures::future::join_all;
+use itertools::Itertools;
 use rand::{Rng, RngCore, SeedableRng};
 use rand_chacha::ChaCha8Rng;
 use tokio::{
@@ -15,6 +16,11 @@ use tracing::{info, subscriber::set_global_default, warn};
 use tracing_subscriber::filter::EnvFilter;
 use types::{TransactionProto, TransactionsClient};
 use url::Url;
+
+// We are distributing the transactions that need to be sent
+// within a second to sub-buckets. The precision here represents
+// the number of such buckets within the period of 1 second.
+const PRECISION: u64 = 20;
 
 #[tokio::main]
 async fn main() -> Result<(), eyre::Report> {
@@ -84,18 +90,26 @@ async fn main() -> Result<(), eyre::Report> {
     // NOTE: This log entry is used to compute performance.
     info!("Transactions rate: {rate} tx/s");
 
+    let mut rng = ChaCha8Rng::seed_from_u64(target.port().unwrap().try_into().unwrap());
+
     let client = Client {
         target,
         size,
         rate,
         nodes,
+        id: rng.next_u64(),
     };
+
+    let txs = client.prepare_transaction(rng);
 
     // Wait for all nodes to be online and synchronized.
     client.wait().await;
 
     // Start the benchmark.
-    client.send().await.context("Failed to submit transactions")
+    client
+        .send(txs)
+        .await
+        .context("Failed to submit transactions")
 }
 
 struct Client {
@@ -103,14 +117,11 @@ struct Client {
     size: usize,
     rate: u64,
     nodes: Vec<Url>,
+    id: u64,
 }
 
 impl Client {
-    pub async fn send(&self) -> Result<(), eyre::Report> {
-        // We are distributing the transactions that need to be sent
-        // within a second to sub-buckets. The precision here represents
-        // the number of such buckets within the period of 1 second.
-        const PRECISION: u64 = 20;
+    pub async fn send(&self, txs: Vec<Vec<TransactionProto>>) -> Result<(), eyre::Report> {
         // The BURST_DURATION represents the period for each bucket we
         // have split. For example if precision is 20 the 1 second (1000ms)
         // will be split in 20 buckets where each one will be 50ms apart.
@@ -138,50 +149,22 @@ impl Client {
             .await
             .context(format!("failed to connect to {}", self.target))?;
 
-        // Submit all transactions.
-        let mut counter = 0;
-        let mut rng = ChaCha8Rng::seed_from_u64(self.target.port().unwrap().try_into().unwrap());
-        let mut r = rng.gen();
-        let client_id: u64 = rng.next_u64();
-        let mut txo: u64 = rng.next_u64();
         let interval = interval(Duration::from_millis(BURST_DURATION));
         tokio::pin!(interval);
+        // create tx before senidng to reduce the load during sending txs.xd
 
+        let mut burst_nb = 0;
         // NOTE: This log entry is used to compute performance.
         info!("Start sending transactions");
 
         'main: loop {
             interval.as_mut().tick().await;
             let now = Instant::now();
-
-            let mut tx = BytesMut::with_capacity(self.size);
-            let size = self.size;
-
-            // move in the following map copy the random value r and txo, so we need to update it outside of map.
-            let stream = tokio_stream::iter(0..burst).map(move |x| {
-                if x == counter % burst {
-                    // NOTE: This log entry is used to compute performance.
-                    info!("Sending sample transaction {}", client_id + counter);
-
-                    tx.put_u8(0u8); // Sample txs start with 0.
-                    tx.put_u64(client_id + counter); // This counter identifies the tx.
-
-                    tx.put_u8(2); // push inputs
-                    tx.put_u64(txo + x);
-                    tx.put_u64(txo + burst);
-                } else {
-                    tx.put_u8(1u8); // Standard txs start with 1.
-                    tx.put_u64(r + x); // Ensures all clients send different txs.
-
-                    tx.put_u8(1); // push inputs
-                    tx.put_u64(txo + x);
-                };
-                tx.resize(size, 0u8);
-                let bytes = tx.split().freeze();
-                TransactionProto { transaction: bytes }
-            });
-
-            if let Err(e) = client.submit_transaction_stream(stream).await {
+            info!("Sending sample transaction {}", self.id + burst_nb as u64);
+            if let Err(e) = client
+                .submit_transaction_stream(tokio_stream::iter(txs[burst_nb].clone()))
+                .await
+            {
                 warn!("Failed to send transaction: {e}");
                 break 'main;
             }
@@ -190,11 +173,7 @@ impl Client {
                 // NOTE: This log entry is used to compute performance.
                 warn!("Transaction rate too high for this client");
             }
-            counter += 1;
-
-            // update random value to makesure all txs are different!
-            txo = rng.next_u64();
-            r = rng.next_u64();
+            burst_nb += 1;
         }
         Ok(())
     }
@@ -213,5 +192,52 @@ impl Client {
             })
         }))
         .await;
+    }
+
+    // NOTE:  We only prepare for test within 100 seconds
+    // we store these transactions in a vector and save the running cost
+    fn prepare_transaction(&self, mut rng: ChaCha8Rng) -> Vec<Vec<TransactionProto>> {
+        let rate = self.rate;
+        let size = self.size;
+        let id = self.id;
+
+        let rounds = 100 * PRECISION;
+        let burst = rate / PRECISION;
+        let mut r = rng.gen();
+        let mut txo: u64 = rng.next_u64();
+
+        let mut tx = BytesMut::with_capacity(size);
+        let mut counter = 0;
+        (0..rounds)
+            .map(|_| {
+                let result = (0..burst)
+                    .map(|x| {
+                        if x == counter % burst {
+                            tx.put_u8(0u8); // Sample txs start with 0.
+                            tx.put_u64(id + counter); // This counter identifies the tx.
+
+                            tx.put_u8(2); // push inputs
+                            tx.put_u64(txo + x);
+                            tx.put_u64(txo + burst);
+                        } else {
+                            tx.put_u8(1u8); // Standard txs start with 1.
+                            tx.put_u64(r + x); // Ensures all clients send different txs.
+
+                            tx.put_u8(1); // push inputs
+                            tx.put_u64(txo + x);
+                        };
+                        tx.resize(size, 0u8);
+                        let bytes = tx.split().freeze();
+                        TransactionProto { transaction: bytes }
+                    })
+                    .collect_vec();
+                counter += 1;
+
+                // update random value to makesure all txs are different!
+                txo = rng.next_u64();
+                r = rng.next_u64();
+                return result;
+            })
+            .collect_vec()
     }
 }
